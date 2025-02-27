11dcab9161b2e0376da0c6646653a8d87491f4ce
Joe Choi <cheongchoi112@gmail.com>
2025-02-27 00:50:08 -0500

    - Add Prettier configuration and refactor code style. - Add Nav bar to the main page

--- app.ctrl.js
+++ app.ctrl.js
@@ -1,33 +1,33 @@
 // include express
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // include the mustache template engine for express
-const mustacheExpress = require("mustache-express");
+const mustacheExpress = require('mustache-express');
 
 // include the model so the controller can use its functions
-const Model = require("./app.model.js");
+const Model = require('./app.model.js');
 
 // create database connection
 Model.makeConnection();
 
 // registers the mustache engine with express
-app.engine("mustache", mustacheExpress());
+app.engine('mustache', mustacheExpress());
 
 // sets mustache to be the view engine
-app.set("view engine", "mustache");
+app.set('view engine', 'mustache');
 
 // sets /views to be the /views folder
 // files should have the extension filename.mustache
-app.set("views", __dirname + "/views");
+app.set('views', __dirname + '/views');
 
-app.get("/", async function (req, res) {
-  console.log("GET /");
+app.get('/', async function (req, res) {
+  console.log('GET /');
   try {
     const businessArray = await Model.getAllBusinesses();
 
-    res.render("main_page", {
-      businesses: businessArra,
+    res.render('main_page', {
+      businesses: businessArray,
     });
   } catch (error) {
     console.error(error);
@@ -40,5 +40,5 @@ app.get(/^(.+)$/, function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log("server listening on port 3000...");
+  console.log('server listening on port 3000...');
 });
